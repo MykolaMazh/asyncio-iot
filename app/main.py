45049c50c7623163e165fcbdf4702831a10d93d8
MykolaMazhula <mykm3ua@gmail.com>
2026-04-29 19:47:04 +0200

    split parallel and sequence programs to be run in a row

--- app/main.py
+++ app/main.py
@@ -20,22 +20,30 @@ async def main() -> None:
 
 
     # create a few programs
-    wake_up_program = [
+    wake_up_program1 = [
         Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(speaker_id, MessageType.SWITCH_ON)
     ]
 
-    sleep_program = [
+    wake_up_program2 = [
+        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")
+    ]
+
+    sleep_program1 = [
         Message(hue_light_id, MessageType.SWITCH_OFF),
         Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
+        Message(toilet_id, MessageType.FLUSH)
     ]
 
-    # run the programs
-    await service.run_program(wake_up_program)
-    await service.run_program(sleep_program)
+    sleep_program2 = [
+        Message(toilet_id, MessageType.CLEAN)
+    ]
+
+
+    await service.run_program(wake_up_program1)
+    await service.run_program(wake_up_program2)
+    await service.run_program(sleep_program1)
+    await service.run_program(sleep_program2)
 
 
 if __name__ == "__main__":
