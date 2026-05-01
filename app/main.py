de3662988d40431e20517a997b775b92dca5d18b
MykolaMazhula <mykm3ua@gmail.com>
2026-05-01 11:11:24 +0200

    set SWITCH_OFF after toilet is clean

--- app/main.py
+++ app/main.py
@@ -32,33 +32,34 @@ async def main() -> None:
 
     await run_sequence(
         run_parallel(
-            service.run_program(
-                [
-                    Message(hue_light_id, MessageType.SWITCH_ON),
-                    Message(speaker_id, MessageType.SWITCH_ON)
-                ]
+            service.run_program([
+                Message(hue_light_id, MessageType.SWITCH_ON),
+                Message(speaker_id, MessageType.SWITCH_ON)
+            ])
+        ),
+
+        service.run_program([
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up"
             )
-        ),
-        service.run_program(
-            [
-                Message(speaker_id,
-                        MessageType.PLAY_SONG,
-                        "Rick Astley - Never Gonna Give You Up")
-            ]
-        ),
-        run_parallel(
-            service.run_program(
-                [
-                    Message(hue_light_id, MessageType.SWITCH_OFF),
-                    Message(speaker_id, MessageType.SWITCH_OFF),
-                    Message(toilet_id, MessageType.FLUSH)
-                ]
-            )
-        ),
-        service.run_program(
-            [
+        ]),
+
+        run_sequence(
+            service.run_program([
+                Message(toilet_id, MessageType.FLUSH)
+            ]),
+            service.run_program([
                 Message(toilet_id, MessageType.CLEAN)
-            ]
+            ])
+        ),
+
+        run_parallel(
+            service.run_program([
+                Message(hue_light_id, MessageType.SWITCH_OFF),
+                Message(speaker_id, MessageType.SWITCH_OFF)
+            ])
         )
     )
 
